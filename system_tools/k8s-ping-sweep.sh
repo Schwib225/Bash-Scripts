cbfd434321416f6bdcbe5fd809842a7d90b4ee1b
Michael Schweigert <42818042+Schwib225@users.noreply.github.com>
2020-06-02 14:54:25 -0600

    Update k8s-ping-sweep.sh

--- system_tools/k8s-ping-sweep.sh
+++ system_tools/k8s-ping-sweep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Choose one of these - must be run inside the cluster since ping won't work outside
 pingName=(`kubectl get pods --all-namespaces -o wide | awk '{print $7}'`)   # All namespaces includes the namespace as a separate column
 pingName=(`kubectl get pods --n $namespaceName -o wide | awk '{print $6}'`)
 
@@ -7,3 +8,19 @@ for i in ${pingName[@]:1};
   do
     ping -c 1 $i;
   done
+
+
+
+## Curl instead ##
+svcIP=(`kubectl get svc --all-namespaces -o wide | awk '{print $5}'`)
+
+for i in ${svcIP[@]:1};
+  do 
+    if [ $i = '<none>' ]; then
+      continue
+    else
+      echo "Attempting to curl $i"
+      curl $i
+    fi
+  done
+      
